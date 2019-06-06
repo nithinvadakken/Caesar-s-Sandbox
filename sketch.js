b9c4985866c8346cac87769f233c1f983c09ec00
Marvin <Marvinesc3@gmail.com>
2019-06-06 18:12:23 -0400

    converting to p5

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
+
+/*
+
+var canvas = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+//----------------------------------------------------------------------------------------------------
+
+//Player Class
+function Player(id, armyArray){
+    this.id = id;
+    this.armyArray = armyArray;
+
+}
+
+Player.prototype.addArmy = function(){
+    //allocates  armies for each player
+    //each army should have 15 divisions, with a different number of troops in each division depending
+    //on the troop's size property
+
+};
+ */
---
+++ troop.js
@@ -0,0 +1,70 @@
+// Troop class
+class Troop {
+    constructor(height, width, health, dmg, range, size, speed) {
+        this.height = height;
+        this.width = width;
+        this.x = window.innerWidth / 2;
+        this.y = window.innerHeight / 2;
+
+        this.health = health;
+        this.range = range;
+        this.dmg = dmg;
+        this.size = size;
+        this.speed = speed;
+    }
+
+
+    teleport(tx, ty) {
+        this.x = tx;
+        this.y = ty;
+    }
+
+    getDistanceToTarget(tx, ty) {
+        return Math.sqrt(Math.pow(this.x - tx, 2) + Math.pow(this.y - ty, 2));
+    }
+
+    attack(troopArray) {
+        for (i = 0; i < troopArray.length; i++) {
+            if (this.getDistanceToTarget(troopArray[i].x, troopArray[i].y) <= this.range) {
+                Troop.health -= dmg;
+            }
+        }
+    }
+
+    drawTroop(color) {
+        ctx.fillStyle = color;
+        ctx.fillRect(window.innerWidth / 2, window.innerHeight / 2, this.width, this.height);
+    }
+
+    move(troopArray) {
+        for (i = 0; i < troopArray.length; i++) {
+            if (this.getDistanceToTarget(troopArray[i].x, troopArray[i].y) <= this.range) {
+                //Move towards that target
+                //Look at old code from first game to get enemy movement
+            }
+        }
+    }
+
+    checkBounds(tx, ty) {
+        if ((this.x <= tx || tx <= this.x + this.width) && (this.y <= ty || ty <= this.y + this.height)) {
+            return true;
+        }
+    }
+}
+
+//----------------------------------------------------------------------------------------------------
+
+//Melee Soldier
+function MeleeSoldier(height, width) {
+    Troop.call(this, height, width, 300, 10, 50);
+}
+
+//----------------------------------------------------------------------------------------------------
+
+// Archer (ranger) Class
+function Archer (height, width) {
+    Troop.call(this, height, width, 150, 50, 35);
+}
+
+
+
