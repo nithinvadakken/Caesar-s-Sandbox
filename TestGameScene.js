61a2f467c24e9d666e204a14cfba68f18311bd63
Nithin Vadakken <nitvad21@bergen.org>
2019-06-06 12:08:54 -0400

    made troop class and 2 types of troops

--- TestGameScene.js
+++ TestGameScene.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
+//----------------------------------------------------------------------------------------------------
+
 // Troop class
 function Troop(height, width, health, dmg, range) {
     this.height = height;
@@ -28,25 +30,22 @@ Troop.prototype.attack = function (Troop) {
     }
 };
 
+Troop.prototype.drawTroop = function (color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(window.innerWidth/2, window.innerHeight/2, this.width, this.height);
+};
+
+//----------------------------------------------------------------------------------------------------
 
 //Melee Soldier
 function MeleeSoldier(height, width) {
     Troop.call(this, height, width, 300, 10, 50);
 }
 
-MeleeSoldier.prototype.drawSoldier = function () {
-    ctx.fillStyle = "#355afa";
-    ctx.fillRect(window.innerWidth/2, window.innerHeight/2, this.width, this.height);
-};
-
+//----------------------------------------------------------------------------------------------------
 
 // Archer (ranger) Class
 function Archer (height, width) {
     Troop.call(this, height, width, 150, 50, 35);
 }
-
-Archer.prototype.drawArcher = function() {
-    ctx.fillStyle = "#ffdd35";
-    ctx.fillRect(window.innerWidth/2, window.innerHeight/2, this.width, this.height);
-};
 
