24b08398a13c01640b213f0824d4144143856942
wonjunlee <jasonw0416@gmail.com>
2019-06-06 10:28:18 -0400

    Archer class

--- TestGameScene.js
+++ TestGameScene.js
@@ -2,7 +2,6 @@ var canvas = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
 
-
 function MeleeSoldier(height, width) {
     this.height = height;
     this.width = width;
@@ -12,3 +11,21 @@ MeleeSoldier.prototype.drawSoldier = function () {
     ctx.fillStyle = "#355afa";
     ctx.fillRect(window.innerWidth/2, window.innerHeight/2, this.width, this.height);
 };
+
+
+// Archer (ranger) Class
+function Archer (height, width) {
+    this.height = height;
+    this.width = width;
+    health = 150;
+}
+
+Archer.prototype.drawArcher = function() {
+    ctx.fillStyle = "#ffdd35";
+    ctx.fillRect(window.innerWidth/2, window.innerHeight/2, this.width, this.height);
+};
+
+Archer.prototype.getDistance = function(){
+
+};
+
