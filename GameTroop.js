15f25c44c7018eef182739849b8615e132fa7fb3
wiktor <wiktorlidwin@gmail.com>
2019-06-13 08:36:03 -0400

    GAME SERVER DONE

--- GameTroop.js
+++ GameTroop.js
@@ -1,167 +1,305 @@
 // Troop class
+
 class GameTroop {
-    constructor(x, y, health, dmg, range, speed, size, name, accuracy) {
+
+    constructor(x, y, health, dmg, range, speed, size, name) {
+
         this.pos = createVector(x, y);
+
         this.health = health;
+
         this.range = range;
+
         this.dmg = dmg;
+
         this.size = size;
+
         this.speed = speed*100;
+
         this.name = name;
-        this.accuracy = accuracy; //number from 0 to 10
-        console.log("Troop Creation: " + this.name);
+
     }
+
+
 
     getDistanceToTarget (tx, ty) {
+
         return Math.sqrt(Math.pow(this.pos.x - tx, 2) + Math.pow(this.pos.y - ty, 2));
+
     }
+
+
 
     attack (enemy) {
-        stroke(color(255, 255, 255));
-        strokeWeight(1);
-        if (random(10) < this.accuracy) {
-            enemy.health -= this.dmg;
-            line(this.pos.x, this.pos.y, enemy.pos.x, enemy.pos.y);
 
-        }
+        stroke(color(255, 255, 255));
+
+        strokeWeight(1);
+
+        enemy.health -= this.dmg;
+
+        line(this.pos.x, this.pos.y, enemy.pos.x, enemy.pos.y);
+
         setTimeout(function(){}, 3000);
+
     }
+
+
 
     movement_heuristic(enemies, allies) {
 
         let terror = 0;
 
-        if (enemies.length < 20 && allies.length < 20) {
+
+
+        if (enemies.length < 10 && allies.length < 10) {
+
             return false;
+
         }
 
+
+
         for (let i=0; i<enemies.length; i++) {
-            if (allies[i] !== undefined && enemies[i] !== undefined) {
-                if (this.getDistanceToTarget(enemies[i].pos.x, enemies[i].pos.y) < 100) {
-                    if (this.name === "Melee" && enemies[i].name==="Archer") {
-                        terror += 2;
-                    } else if (this.name === "Archer" && enemies[i].name==="Tank") {
-                        terror += 1;
-                    } else if (this.name === "Melee" && enemies[i].name==="Tank") {
-                        terror += 0.5;
-                    }
+
+            if (this.getDistanceToTarget(enemies[i].pos.x, enemies[i].pos.y) < 100) {
+
+                if (this.name === "Melee" && enemies[i].name==="Archer") {
+
+                    terror += 2;
+
+                } else if (this.name === "Archer" && enemies[i].name==="Tank") {
+
+                    terror += 1;
+
+                } else if (this.name === "Melee" && enemies[i].name==="Tank") {
+
+                    terror += 0.5;
+
                 }
+
             }
+
         }
+
+
 
         for (let i=0; i<allies.length; i++) {
-            if (allies[i] !== undefined && enemies[i] !== undefined) {
-                if (this.getDistanceToTarget(allies[i].pos.x, allies[i].pos.y) < 100) {
-                    if (this.name === "Melee" && enemies[i].name==="Archer") {
-                        terror -= 0.5;
-                    } else if (this.name === "Archer" && enemies[i].name==="Tank") {
-                        terror -= 1;
-                    } else if (this.name === "Melee" && enemies[i].name==="Tank") {
-                        terror -= 2;
-                    }
+
+            if (this.getDistanceToTarget(allies[i].pos.x, allies[i].pos.y) < 100) {
+
+                if (this.name === "Melee" && allies[i].name==="Archer") {
+
+                    terror -= 0.5;
+
+                } else if (this.name === "Archer" && allies[i].name==="Tank") {
+
+                    terror -= 1;
+
+                } else if (this.name === "Melee" && allies[i].name==="Tank") {
+
+                    terror -= 2;
+
                 }
+
             }
+
         }
+
+
 
         if (terror <= 0) {
+
             return false;
+
         } else {
+
             return true;
+
         }
+
     }
+
+
 
     autoMove(enemies, allies) {
+
         let ex = 1000000;
+
         let ey = 1000000;
-        
+
+
+
         for (let i=0; i<enemies.length; i++) {
-            
+
+
+
             let terror = this.movement_heuristic(enemies, allies);
 
+
+
             if (this.getDistanceToTarget(enemies[i].pos.x, enemies[i].pos.y) < this.range) {
+
                 this.attack(enemies[i]);
+
                 break;
+
             } else if (this.getDistanceToTarget(enemies[i].pos.x, enemies[i].pos.y) < this.getDistanceToTarget(ex, ey)) {
-                ex =  enemies[i].pos.x; 
+
+                ex =  enemies[i].pos.x;
+
                 ey = enemies[i].pos.y;
+
                 this.targetMove(ex, ey, terror);
+
             }
+
         }
+
     }
+
+
 
     targetMove(tx, ty, terror) {
+
         let xspeed = (tx - this.pos.x)/this.speed;
+
         let yspeed = (ty - this.pos.y)/this.speed;
 
+
+
         if (terror) {
+
             xspeed = (this.pos.x - tx)/this.speed;
+
             yspeed = (this.pos.y - ty)/this.speed;
+
         }
+
+
 
         if (this.pos.x + xspeed < 0) {
+
             this.pos.x = 0;
+
         } else if (this.pos.x + xspeed > window.innerWidth) {
+
             this.pos.x = window.innerWidth;
+
         } else {
+
             this.pos.x += xspeed;
+
         }
+
+
 
         if (this.pos.y + yspeed < 0) {
+
             this.pos.y = 0;
+
         } else if (this.pos.y + yspeed > window.innerHeight) {
+
             this.pos.y = window.innerHeight;
+
         } else {
+
             this.pos.y += yspeed;
+
         }
+
     }
+
 }
+
+
 
 //Melee Class
+
 class MeleeSoldier extends GameTroop {
-    //x, y, health, dmg, range, speed, size, name, acc
-    constructor(x, y) {
-        super(x, y, 300, 30, 30, 10, 4, "Melee", 10);
+
+    constructor(x, y, name) {
+
+        super(x, y, 300, 30, 40, 10, 7, "Melee");
+
     }
 
+
+
     drawTroop(clr){
+
         strokeWeight(8);
+
         stroke(clr);
-        fill(clr);
+
         ellipse(this.pos.x, this.pos.y, this.size);
+
     }
+
 }
+
+
 
 // Archer (ranger) Class
+
 class Archer extends GameTroop {
-    //x, y, health, dmg, range, speed, size, name, acc
-    constructor(x, y) {
+
+    constructor(x, y, name) {
+
         //yellow
-        super(x, y, 200, 15, 60, 50, 20, "Archer", 5);
+
+        super(x, y, 200, 15, 70, 30, 30, "Archer");
+
     }
 
+
+
     drawTroop(clr){
+
         strokeWeight(8);
+
         stroke(clr);
-        fill(clr);
+
         let x = this.pos.x;
+
         let y = this.pos.y;
+
         triangle(x, y, x+2, y, (x + (x+2))/2, y-2);
+
     }
+
 }
 
+
+
 // Tank Class
+
 class Tank extends GameTroop {
+
     constructor(x, y) {
+
         //x, y, health, dmg, range, speed, size, name, acc
-        super(x, y, 1000, 40, 45, 20, 30, "Tank", 6);
+
+        super(x, y, 1000, 40, 45, 50, 40, "Tank");
+
     }
 
+
+
     drawTroop(clr){
+
         strokeWeight(8);
+
         stroke(clr);
+
         fill(clr);
+
         let x = this.pos.x;
+
         let y = this.pos.y;
+
         rect(x, y, this.size, this.size);
+
     }
+
 }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let player;
+let start = false;
 
 function setup() {
     // put setup code here
@@ -16,19 +17,47 @@ function setup() {
 //     player2.update();
 // }
 
+function keyPressed(){
+    if (keyCode === 81) {
+        player.addArmyOnClick(mouseX,mouseY,0);
+    }
+    else if (keyCode === 87) {
+        player.addArmyOnClick(mouseX,mouseY,1);
+    }
+    else if (keyCode === 69) {
+        player.addArmyOnClick(mouseX,mouseY,2);
+    }
+
+    else if (keyCode === 73) {
+        player2.addArmyOnClick(mouseX,mouseY,0);
+    }
+    else if (keyCode === 79) {
+        player2.addArmyOnClick(mouseX,mouseY,1);
+    }
+    else if (keyCode === 80) {
+        player2.addArmyOnClick(mouseX,mouseY,2);
+    }
+    else if (keyCode === UP_ARROW) {
+        start = true;
+    }
+}
+
 function draw() {
     // put drawing code here
-    if (player.numTroops <= 0 || player2.numTroops <= 0) {
+    /*if (player.numTroops <= 0 || player2.numTroops <= 0) {
         window.location.reload();
-    }
+    }*/
     background(0);
-    translate(window.innerWidth/2-player.position.x, window.innerHeight/2-player.position.y);
+    //translate(window.innerWidth/2-player.position.x, window.innerHeight/2-player.position.y);
     player.show();
     player2.show();
-    player.update();
-    player2.update();
-    player.moveArmy();
-    player2.moveArmy();
+    if (start === true){
+        player.update();
+        player2.update();
+        player.moveArmy();
+        player2.moveArmy();
+    }
+
 }
 
 function mousePressed(){
@@ -36,4 +65,3 @@ function mousePressed(){
         // player.clicked(); //not finished
     }
 }
-
