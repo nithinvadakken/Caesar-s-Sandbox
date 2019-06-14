175d0f56d88949e1d0cebea2b2e3f279314981b6
Nithin Vadakken <nitvad21@bergen.org>
2019-06-14 11:14:14 -0400

    gg

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express'),
 app.use('/', express.static(__dirname + '/www'));
 //bind the server to the 80 port
 //server.listen(3000);//for local test
-server.listen(/*process.env.PORT */ 80);//publish to heroku
+server.listen(process.env.PORT  || 3000);//publish to heroku
 //server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
 //console.log('server started on port'+process.env.PORT || 3000);
 //handle the socket
